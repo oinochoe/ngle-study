e43498a983492534bd8a546eff4e1640c1957e53
Yeongmin Kim <copstyle86@gmail.com>
2018-08-14 15:11:24 +0900

    Change from `IF` to JunitCore

--- springbook/src/test/java/springbook/user/UserDaoTest.java
+++ springbook/src/test/java/springbook/user/UserDaoTest.java
@@ -1,39 +1,41 @@
 package springbook.user;
 
+import springbook.user.domain.User;
+import springbook.user.dao.UserDao;
+import java.sql.SQLException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import springbook.user.dao.UserDao;
-import springbook.user.domain.User;
 
-import java.sql.SQLException;
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 
 public class UserDaoTest {
     @Test
     public void addAndGet() throws SQLException {
+
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("Triumph");
-        user.setName("김영민");
-        user.setPassword("married");
+        user.setId("noel");
+        user.setName("Yeongmin");
+        user.setPassword("toby");
 
         dao.add(user);
 
         User user2 = dao.get(user.getId());
 
-        if (!user.getId().equals(user2.getId())) {
-            System.out.println("테스트 실패 (Id)");
-        } else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (Password)");
-        } else if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (Name)");
-        } else {
-            System.out.println("조회테스트 성공");
-        }
+        assertThat(user2.getId(), is(user.getId()));
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
 
+    }
+
+    public static void main(String[] args) {
+        JUnitCore.main("springbook.user.UserDaoTest");
     }
 }
