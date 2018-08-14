c9cbef8adefa85965a274ef58f5a7faa7188e630
Yeongmin Kim <copstyle86@gmail.com>
2018-08-14 14:31:06 +0900

    Doing Test and Junit Test

--- springbook/src/test/java/springbook/user/UserDaoTest.java
+++ springbook/src/test/java/springbook/user/UserDaoTest.java
@@ -2,6 +2,7 @@ package springbook.user;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.DaoFactory;
@@ -9,36 +10,39 @@ import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
+import org.junit.Test;
+
 
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException {
-
+    @Test
+    public void addAndGet() throws SQLException {
         //Daofactory 사용
         //ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
         //GenericXmlApplicationContext 사용 해서 스프링 API 대신 xml화 한다.
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-
         user.setId("Triumph");
         user.setName("김영민");
         user.setPassword("married");
 
         dao.add(user);
 
-        System.out.println(user.getId() + "등록성공");
-
         User user2 = dao.get(user.getId());
 
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + "조회 성공");
-
-
+        if (!user.getId().equals(user2.getId())) {
+            System.out.println("테스트 실패 (Id)");
+        } else if (!user.getPassword().equals(user2.getPassword())) {
+            System.out.println("테스트 실패 (Password)");
+        } else if (!user.getName().equals(user2.getName())) {
+            System.out.println("테스트 실패 (Name)");
+        } else {
+            System.out.println("조회테스트 성공");
+        }
 
     }
 }
