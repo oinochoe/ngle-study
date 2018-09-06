53f1ca00a7449b11e25f228f5307713ec8203393
Yeongmin Kim <copstyle86@gmail.com>
2018-09-06 18:04:27 +0900

    클라이언트책임을 갖도록 변경한 deleteAll()

--- springbook/src/main/java/springbook/user/dao/UserDao.java
+++ springbook/src/main/java/springbook/user/dao/UserDao.java
@@ -60,23 +60,10 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
 
-        try {
-            c = dataSource.getConnection();
+            StatementStrategy st = new DeleteAllStatement(); // 선정한 전략 클래스의 오브젝트 생성
+            jdbcContextWithStatementStrategy(st); // 컨텍스트 호출. 전략 오브젝트 전달
 
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-
-            ps.executeUpdate();
-
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) { try {ps.close();} catch (SQLException e) {} }
-            if (c != null) { try {c.close();} catch (SQLException e) {} }
-        }
     }
 
     public int getCount() throws SQLException {
@@ -100,4 +87,21 @@ public class UserDao {
         }
     }
 
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        // 클라이언트가 컨텍셔트를 호출할 때 넘겨줄 파라미터 :  StatementStrategy stmt
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
+            if (c != null) { try { c.close(); } catch (SQLException e) {} }
+        }
+    }
 }
