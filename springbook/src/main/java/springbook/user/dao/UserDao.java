0425df38614f1bc8f95445637dd257f9f031e9a4
yeongmin kim <copstyle86@gmail.com>
2018-09-16 18:37:34 +0900

    JdbcTemplate(뒤통수).. 너무 간결해지는거 아닌가.

--- springbook/src/main/java/springbook/user/dao/UserDao.java
+++ springbook/src/main/java/springbook/user/dao/UserDao.java
@@ -1,6 +1,8 @@
 package springbook.user.dao;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import springbook.user.domain.User;
 
 import java.sql.*;
@@ -10,31 +12,16 @@ import javax.sql.DataSource;
 public class UserDao {
 
     private DataSource dataSource;
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
-    public void setDataSource(DataSource dataSource) { // 수정자 메소드이면서 JdbcContext에 대한 생성, DI 작업을 동시에 수행
-        this.jdbcContext = new JdbcContext(); // JdbcContext 생성(IOC)
-        this.jdbcContext.setDataSource(dataSource); // 의존 오브젝트 주입(DI)
-        this.dataSource = dataSource; // 아직 JdbcContext를 적용하지 않은 메소드를 위해 저장해둔다.
-        // 먼저 JdbcContext의 오브젝트를 만들어서 인스턴스 변수에 저장해두고, JdbcContext에 UserDao가 DI 받은
-        // DataSource 오브제를 주입해주면 완벽한 DI가 완성된다.
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.dataSource = dataSource;
     }
 
-
     public void add(final User user) throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(
-        new StatementStrategy() {
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement(
-                            "insert into tbl_users(id, name, password) values(?,?,?)");
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getName());
-                    ps.setString(3, user.getPassword());
-
-                    return ps;
-                }
-            }
-        );
+        this.jdbcTemplate.update("insert into tbl_users(id, name, password) values(?,?,?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) throws SQLException {
@@ -65,8 +52,20 @@ public class UserDao {
         return user;
     }
 
-    public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from tbl_users");
+    // JdbcTemplate 사용
+    /*public void deleteAll() {
+        this.jdbcTemplate.update(
+            new PreparedStatementCreator() {
+                public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                    return con.prepareStatement("delete from tbl_users");
+                }
+            }
+        );
+    }*/
+
+    // 위 보다 더 단순한 JdbcTemplate의 내장 콜백 사용
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from tbl_users");
     }
 
 
