74a2443036d9ffa97e146523b8f571dbb41ddfc2
yeongmin kim <copstyle86@gmail.com>
2018-09-16 17:39:20 +0900

    FilePath 까다롭네. 일단 BufferReader 템플릿/콜백사용으로 2개의 test 사용 완료

--- springbook/src/test/java/springbook/learningtest/template/CalcSumTest.java
+++ springbook/src/test/java/springbook/learningtest/template/CalcSumTest.java
@@ -1,17 +1,28 @@
 package springbook.learningtest.template;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import java.io.IOException;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CalcSumTest {
-    @Test
-    public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        //int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath()); 사용이 되지 않아 주석처리.
-        int sum = calculator.calcSum("C:\\Users\\nGle\\git\\web\\springbook\\src\\test\\java\\springbook\\learningtest\\template\\numbers.txt");
-        assertThat(sum, is(9)); // 10으로 바꾸면 해결된다.
-        System.out.println(sum);
+    Calculator calculator;
+    String numFilepath;
+
+    @Before
+    public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = "F:\\web\\springbook\\src\\test\\java\\springbook\\learningtest\\template\\numbers.txt";
+        //int sum = calculator.calcSum("C:\\Users\\nGle\\git\\web\\springbook\\src\\test\\java\\springbook\\learningtest\\template\\numbers.txt");
+    }
+
+    @Test public void sumOfNumbers() throws IOException {
+        assertThat(calculator.calcSum(this.numFilepath), is(10));
+    }
+
+    @Test public void multiplyOfNumbers() throws IOException {
+        assertThat(calculator.calcMultiply(this.numFilepath), is(24));
     }
 }
--- springbook/src/test/java/springbook/learningtest/template/Calculator.java
+++ springbook/src/test/java/springbook/learningtest/template/Calculator.java
@@ -6,28 +6,49 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReader br = null;
+        BufferedReaderCallback sumCallback =
+            new BufferedReaderCallback() {
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer sum = 0;
+                String line = null;
+                while((line = br.readLine()) != null) {
+                    sum += Integer.valueOf(line);
+                }
+                return sum;
+            }
+        };
+        return fileReadTemplate(filepath, sumCallback);
+    }
 
+    public Integer calcMultiply(String filepath) throws IOException {
+        BufferedReaderCallback multiplyCallback =
+        new BufferedReaderCallback() {
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer multiply = 1;
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    multiply *= Integer.valueOf(line);
+                }
+                return multiply;
+            }
+        };
+        return fileReadTemplate(filepath, multiplyCallback);
+    }
+
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+        BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(filepath));
-            // 한 줄 씩 읽기 편하게 BufferReader로 파일을 가져온다.
-            Integer sum = 0;
-            String line = null;
-            while ((line=br.readLine()) != null) { // 마지막 라인까지 한줄씩 읽어가면서 숫자를 더한다.
-                sum = sum + Integer.valueOf(line);
-            }
-            br.close();
-            return sum;
-        }
-        catch (IOException e) {
+            int ret = callback.doSomethingWithReader(br);
+            // 콜백 오브젝트 호출.. 템플릿에서 만든 컨텍스트 정보인 BufferedReader를 전달해주고 콜백의 작업결과를 받아둔다.
+            return ret;
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;
-        }
-        finally {
-            if (br != null) { // BufferedReader 오브젝트가 생성되기 전에 예외가 발생할 수도 있으므로 반드시 null 체크를 먼저 해야한다.
-                try {
-                    br.close();
-                } catch (IOException e) {
+        } finally {
+            if (br != null) {
+                try {br.close();}
+                catch (IOException e) {
                     System.out.println(e.getMessage());
                 }
             }
