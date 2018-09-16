d6ebc232f16363a05d85dfd332f7a52b549deefd
oinochoe <oinochoe@naver.com>
2018-09-17 00:13:32 +0900

    Datasource를 전달만 받도록 수정자메소드만 남겨두고 제거 중복제거를 위해 userMapper 메소드 추가해서 공유.

--- springbook/src/main/java/springbook/user/dao/UserDao.java
+++ springbook/src/main/java/springbook/user/dao/UserDao.java
@@ -11,37 +11,15 @@ import javax.sql.DataSource;
 
 public class UserDao {
 
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.dataSource = dataSource;
+        // DataSource 오브젝트는 JdbcTemplate을 만든 후에는 사용하지 않으니 저장해두지 않아도 된다.
     }
 
-    public void add(final User user) throws SQLException {
-        this.jdbcTemplate.update("insert into tbl_users(id, name, password) values(?,?,?)",
-                user.getId(), user.getName(), user.getPassword());
-    }
-
-    public User get(String id) {
-        return this.jdbcTemplate.queryForObject("select * from tbl_users where id = ?", new Object[]{id},
-            //sql 바인딩할 파라미터 값. 가변인자 대신 배열을 사용.
-            new RowMapper<User>() {
-                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    User user = new User();
-                    user.setId(rs.getString("id"));
-                    user.setName(rs.getString("name"));
-                    user.setPassword(rs.getString("password"));
-                    return user;
-                }
-            } // Resultset한 로우의 결과를 오브젝트에 매핑해주는 RowMapper 콜백
-        );
-    }
-
-    public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from tbl_users order by id",
-            new RowMapper<User>() {
+    private RowMapper<User> userMapper =
+        new RowMapper<User>() {
             public User mapRow(ResultSet rs, int rowNum) throws SQLException {
                 User user = new User();
                 user.setId(rs.getString("id"));
@@ -49,7 +27,19 @@ public class UserDao {
                 user.setPassword(rs.getString("password"));
                 return user;
             }
-        });
+        };
+
+    public void add(final User user) throws SQLException {
+        this.jdbcTemplate.update("insert into tbl_users(id, name, password) values(?,?,?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject("select * from tbl_users where id = ?", new Object[]{id}, this.userMapper);
+    }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from tbl_users order by id", this.userMapper);
     }
 
     public void deleteAll() {
@@ -58,23 +48,5 @@ public class UserDao {
 
     public int getCount() {
         return this.jdbcTemplate.queryForInt("select count(*) from tbl_users");
-    }
-
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        // 클라이언트가 컨텍셔트를 호출할 때 넘겨줄 파라미터 :  StatementStrategy stmt
-
-        try {
-            c = dataSource.getConnection();
-            ps = stmt.makePreparedStatement(c);
-            ps.executeUpdate();
-
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
-            if (c != null) { try { c.close(); } catch (SQLException e) {} }
-        }
     }
 }
