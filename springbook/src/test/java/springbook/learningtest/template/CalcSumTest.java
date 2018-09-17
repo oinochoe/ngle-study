28bf455052627cf5fe1df1434d98095d95cf4c82
oinochoe <oinochoe@naver.com>
2018-09-17 10:48:15 +0900

    Edit Confilict

--- springbook/src/test/java/springbook/learningtest/template/CalcSumTest.java
+++ springbook/src/test/java/springbook/learningtest/template/CalcSumTest.java
@@ -8,15 +8,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CalcSumTest {
-<<<<<<< HEAD
-    @Test
-    public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        //int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath()); 사용이 되지 않아 주석처리.
-        int sum = calculator.calcSum("C:\\Users\\nGle\\git\\web\\springbook\\src\\test\\java\\springbook\\learningtest\\template\\numbers.txt");
-        assertThat(sum, is(10)); // 10으로 바꾸면 해결된다.
-        System.out.println(sum);
-=======
     Calculator calculator;
     String numFilepath;
 
@@ -37,6 +28,5 @@ public class CalcSumTest {
 
     @Test public void concatenateStrings() throws IOException {
         assertThat(calculator.concatenate(this.numFilepath), is("1234"));
->>>>>>> d6ebc232f16363a05d85dfd332f7a52b549deefd
     }
 }
