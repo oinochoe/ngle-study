92ac3e73b669f27c739cc4000c98b51a124110b2
oinochoe <oinochoe@naver.com>
2018-09-29 19:45:07 +0900

    statController 복구 및 Spring ServletContext 사용한 방법으로 진행중

---
+++ mybatis/src/main/java/com/mybatis/board/VO/ExcelVO.java
@@ -0,0 +1,25 @@
+package com.mybatis.board.VO;
+
+import lombok.Data;
+
+@Data
+public class ExcelVO {
+    private String bsrlid;
+    private String bsrlnm;
+
+   /* public String getBsrlid() {
+        return bsrlid;
+    }
+
+    public void setBsrlid(String bsrlid) {
+        this.bsrlid = bsrlid;
+    }
+
+    public String getBsrlnm() {
+        return bsrlnm;
+    }
+
+    public void setBsrlnm(String bsrlnm) {
+        this.bsrlnm = bsrlnm;
+    }*/
+}
